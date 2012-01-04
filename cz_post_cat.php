058f087b4c64c5ca90d1bb4b54302707eb74b4a6
zevdesigns <zevdesigns@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-04 14:49:16 +0000

    v1.0
    git-svn-id: https://plugins.svn.wordpress.org/posts-categories-in-sidebar/trunk@484514 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cz_post_cat.php
@@ -0,0 +1,65 @@
+<?php
+/*
+Plugin Name: Post's Categories in Sidebar
+Plugin URI: #
+Description: Displays the Categories for a single post, as a widget.
+Author: Corey Zev
+Version: 1.0
+Author URI: http://zevdesigns.com
+    
+*/
+
+class Post_Cat_Widget extends WP_Widget {
+
+	function Post_Cat_Widget() {
+		$widget_ops = array('classname' => 'widget_cz_post_cat', 'description' => __('Displays the Categories for a single post, as a widget.'));
+		$control_ops = array('width' => 200, 'height' => 200);
+		$this->WP_Widget('cz_post_cat', __("Post's Categories in Sidebar"), $widget_ops, $control_ops);
+	}
+
+	function widget( $args, $instance ) {
+		extract($args);
+		if(is_single()){
+		// Would not work on any other page than the "Single"
+			$title = apply_filters( 'widget_title', empty($instance['title']) ? '' : $instance['title'], $instance );
+			echo $before_widget;
+			echo $before_title . $title . $after_title;
+			echo '<div class="cz-post-cats">';
+		 	global $post;
+			$sing_categories = get_the_category($post->ID);
+			//Display The Post's Categories
+			echo '<ul class="cz-cat-ul">';
+			foreach($sing_categories as $category) { 
+			echo '<li class="cz-cat-li">';
+			echo '<a href="'.get_category_link($category->term_id ).'">'.$category->cat_name.'</a>';
+			echo '</li>';
+			}		
+			echo '</ul>';	
+			echo '</div>';
+		echo $after_widget;
+		}
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		if ( current_user_can('unfiltered_html') )
+			$instance['text'] =  $new_instance['text'];
+		else
+			$instance['text'] = stripslashes( wp_filter_post_kses( $new_instance['text'] ) );
+		$instance['filter'] = isset($new_instance['filter']);
+		return $instance;
+	}
+
+	function form( $instance ) {
+		$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'text' => '' ) );
+		$title = strip_tags($instance['title']);
+?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
+		
+<?php
+	}
+}
+
+add_action('widgets_init', create_function('', 'return register_widget("Post_Cat_Widget");'));
---
+++ readme.txt
@@ -0,0 +1,68 @@
+=== Plugin Name ===
+Contributors: zevdesigns
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=3TM2KKRHGJZDG&lc=US&item_name=ZevDesigns&item_number=wp%2dcat%2dpost&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted
+Tags: categories, sidebar, widget, post, single, page, specific, only, category
+Requires at least: 2.0
+Tested up to: 3.3
+Stable tag: 1.0
+
+Displays the categories for a single post in the sidebar.
+
+== Description ==
+
+This plugin was created to show the categories for a single post in the sidebar.
+
+The categories will be displayed in an unordered list.
+
+You can choose the title for the widget in the sidebar if you wish.
+
+Example:
+
+	<li id="cz_post_cat-3" class="widget-container widget_cz_post_cat">
+		<h3 class="widget-title">Topics</h3>
+		<div class="cz-post-cats">
+			<ul class="cz-cat-ul">
+				<li class="cz-cat-li"><a href="http://xxx.xxx.xxx/topic/facebook/">Facebook</a></li>
+				<li class="cz-cat-li"><a href="http://xxx.xxx.xxx/topic/jobs/">Jobs</a></li>
+			</ul>
+		</div>
+	</li>
+
+It uses the `is_single()` query in php so that it ONLY shows up on single posts. Not pages.
+
+== Installation ==
+
+Very easy.
+
+1. Upload `post-cat-widget.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Place the Widget in your sidebar wherever you desire.
+4. Title it whatever you want.
+
+The div's in the widget have been labeld with very easy classes for styling in your CSS.
+
+== Frequently Asked Questions ==
+
+First version. No questions yet.
+
+== Screenshots ==
+
+1. The widget in use. Styled according to the normal widget sidebar.
+2. The Application -> Widgets page
+
+== Changelog ==
+
+= 1.0 =
+* First version.
+* Pure awesome.
+
+== Upgrade Notice ==
+
+= 1.0 =
+* First version.
+* Pure awesome.
+
+
+== Arbitrary section ==
+
+Anyone need any info here?
